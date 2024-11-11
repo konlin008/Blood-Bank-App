820af15064f0988186e4d26fa1211c32b0c953d3
Aman Mondal <162113539+konlin008@users.noreply.github.com>
2024-11-11 17:01:43 +0530

    Your commit message here

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
-import HomePages from './pages/HomePages';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
+import { Routes, Route } from "react-router-dom";
+import HomePages from "./pages/HomePages";
+import Register from "./pages/auth/Register";
+import Loggin from "./pages/auth/Loggin";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<HomePages />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+        <Route path="/" element={<HomePages />} />
+        <Route path="/login" element={<Loggin />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
