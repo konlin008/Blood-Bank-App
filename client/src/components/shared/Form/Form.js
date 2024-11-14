b47cc3e7e42955a540c97cedb71cf26dde58ad5b
Aman Mondal <162113539+konlin008@users.noreply.github.com>
2024-11-14 23:25:03 +0530

    created Reusable Login Register Form

--- client/src/components/shared/Form/Form.js
+++ client/src/components/shared/Form/Form.js
@@ -1,14 +1,127 @@
 import React, { useState } from "react";
 import InputType from "./InputType";
-const Form = ({ submitBtn, formTitle }) => {
+const Form = ({ formType, submitBtn, formTitle }) => {
   const [email, setEmail] = useState("");
-  const [password, setpassword] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("donar");
+  const [name, setName] = useState("");
+  const [organisationName, setOrganisationName] = useState("");
+  const [hospitalName, setHospitalName] = useState("");
+  const [website, setWebsite] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
   return (
     <div>
       <form>
         <h1 className="text-center">{formTitle}</h1>
         <hr />
-        <InputType
+        {(() => {
+          switch (true) {
+            case formType === "login": {
+              return (
+                <>
+                  <InputType
+                    LabelText={"Email"}
+                    LableFor={"forEmail"}
+                    inputType={"email"}
+                    name={"email"}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <InputType
+                    LabelText={"PassWord"}
+                    LableFor={"forPassWord"}
+                    inputType={"password"}
+                    name={"password"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </>
+              );
+            }
+            case formType === "register": {
+              return (
+                <>
+                  <InputType
+                    LabelText={"Email"}
+                    LableFor={"forEmail"}
+                    inputType={"email"}
+                    name={"email"}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <InputType
+                    LabelText={"PassWord"}
+                    LableFor={"forPassWord"}
+                    inputType={"password"}
+                    name={"password"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <InputType
+                    LabelText={"Name"}
+                    LableFor={"forName"}
+                    inputType={"text"}
+                    name={"name"}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <InputType
+                    LabelText={"Role"}
+                    LableFor={"forRole"}
+                    inputType={"text"}
+                    name={"role"}
+                    value={role}
+                    onChange={(e) => setRole(e.target.value)}
+                  />
+                  <InputType
+                    LabelText={"OrganisationName"}
+                    LableFor={"forOrganisationName"}
+                    inputType={"text"}
+                    name={"organisationName"}
+                    value={organisationName}
+                    onChange={(e) => setOrganisationName(e.target.value)}
+                  />
+                  <InputType
+                    LabelText={"HospitalName"}
+                    LableFor={"forHospitalName"}
+                    inputType={"text"}
+                    name={"hospitalName"}
+                    value={hospitalName}
+                    onChange={(e) => setHospitalName(e.target.value)}
+                  />
+                  <InputType
+                    LabelText={"Website"}
+                    LableFor={"forWebsite"}
+                    inputType={"text"}
+                    name={"website"}
+                    value={website}
+                    onChange={(e) => setWebsite(e.target.value)}
+                  />
+                  <InputType
+                    LabelText={"Address"}
+                    LableFor={"forAddress"}
+                    inputType={"text"}
+                    name={"address"}
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                  />
+                  <InputType
+                    LabelText={"Phone"}
+                    LableFor={"forPhone"}
+                    inputType={"text"}
+                    name={"phone"}
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </>
+              );
+            }
+            default:
+              return null;
+          }
+        })()}
+        {/* <InputType
           LabelText={"Email"}
           LableFor={"forEmail"}
           inputType={"email"}
@@ -22,10 +135,66 @@ const Form = ({ submitBtn, formTitle }) => {
           inputType={"passWord"}
           name={"passWord"}
           value={password}
-          onChange={(e) => setpassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value)}
         />
+        <InputType
+          LabelText={"Name"}
+          LableFor={"forName"}
+          inputType={"text"}
+          name={"name"}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <InputType
+          LabelText={"Role"}
+          LableFor={"forRole"}
+          inputType={"text"}
+          name={"role"}
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
+        />
+        <InputType
+          LabelText={"OrganisationName"}
+          LableFor={"forOrganisationName"}
+          inputType={"text"}
+          name={"organisationName"}
+          value={organisationName}
+          onChange={(e) => setOrganisationName(e.target.value)}
+        />
+        <InputType
+          LabelText={"HospitalName"}
+          LableFor={"forHospitalName"}
+          inputType={"text"}
+          name={"hospitalName"}
+          value={hospitalName}
+          onChange={(e) => setHospitalName(e.target.value)}
+        />
+        <InputType
+          LabelText={"Website"}
+          LableFor={"forWebsite"}
+          inputType={"text"}
+          name={"website"}
+          value={website}
+          onChange={(e) => setWebsite(e.target.value)}
+        />
+        <InputType
+          LabelText={"Address"}
+          LableFor={"forAddress"}
+          inputType={"text"}
+          name={"address"}
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <InputType
+          LabelText={"Phone"}
+          LableFor={"forPhone"}
+          inputType={"text"}
+          name={"phone"}
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        /> */}
         <div className="d-felx">
-          <button className="btn btn-pimary" type="submit">
+          <button class="btn btn-primary" type="submit">
             {submitBtn}
           </button>
         </div>
--- client/src/pages/auth/Loggin.js
+++ client/src/pages/auth/Loggin.js
@@ -4,12 +4,16 @@ import Form from "../../components/shared/Form/Form";
 const Loggin = () => {
   return (
     <>
-      <div className="row">
+      <div className="row g-0">
         <div className="col-md-8 form-banner">
           <img src="./assets/images/banner1.jpg" alt="loginImage"></img>
         </div>
         <div className="col-md-4 form-container">
-          <Form formTitle={"Login Page"} submitBtn={"Login"} />
+          <Form
+            formTitle={"Login Page"}
+            submitBtn={"Login"}
+            formType={"login"}
+          />
         </div>
       </div>
     </>
